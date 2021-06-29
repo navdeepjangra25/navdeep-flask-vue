87636383924d0aada5cb5b09970a1d3635bbd276
Navdeep-Jangra-at-294649740626 <navdeep.jangra@nmgtechnologies.com6>
2021-06-29 23:30:44 +0530

    added node version package.json

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js + Flask App"/>
+    <HelloWorld msg="Welcome to your first Vue.js + Flask App"/>
   </div>
 </template>
 
